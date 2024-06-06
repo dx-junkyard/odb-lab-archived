a0be562638ecc5c69bc902f4fbb4386b84fe93c1
fooqoo <horseshoes.buzz.17@gmail.com>
2024-06-07 08:46:52 +0900

    feat: デジタル庁デザインシステムの採用

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.scss';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,7 +9,7 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 };
--- src/components/pages/StoryPage.tsx
+++ src/components/pages/StoryPage.tsx
@@ -1,8 +1,17 @@
 'use client';
 import React, { FC, useState } from 'react';
 
-import { Header } from '../organizms/header/Header';
 import './story-page.scss';
+import {
+  Accordion,
+  AccordionDefaultIcon,
+  AccordionSummary,
+} from '../digital-go-jp/accordion/Accordion';
+import {
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Breadcrumbs,
+} from '../digital-go-jp/breadcrumbs/Breadcrumbs';
 
 type User = {
   name: string;
@@ -13,80 +22,40 @@ export const StoryPage: FC = () => {
 
   return (
     <article>
-      <Header
-        user={user}
-        onLogin={() => setUser({ name: 'Jane Doe' })}
-        onLogout={() => setUser(undefined)}
-        onCreateAccount={() => setUser({ name: 'Jane Doe' })}
-      />
-
-      <section className="storybook-page">
-        <h2>Pages in Storybook</h2>
-        <p>
-          We recommend building UIs with a{' '}
-          <a
-            href="https://componentdriven.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <strong>component-driven</strong>
-          </a>{' '}
-          process starting with atomic components and ending with pages.
-        </p>
-        <p>
-          Render pages with mock data. This makes it easy to build and review
-          page states without needing to navigate to them in your app. Here are
-          some handy patterns for managing page data in Storybook:
-        </p>
-        <ul>
-          <li>
-            Use a higher-level connected component. Storybook helps you compose
-            such data from the &quot;args&quot; of child component stories
-          </li>
-          <li>
-            Assemble data in the page component from your services. You can mock
-            these services out using Storybook.
-          </li>
-        </ul>
-        <p>
-          Get a guided tutorial on component-driven development at{' '}
-          <a
-            href="https://storybook.js.org/tutorials/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Storybook tutorials
-          </a>
-          . Read more in the{' '}
-          <a
-            href="https://storybook.js.org/docs"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            docs
-          </a>
-          .
-        </p>
-        <div className="tip-wrapper">
-          <span className="tip">Tip</span> Adjust the width of the canvas with
-          the{' '}
-          <svg
-            width="10"
-            height="10"
-            viewBox="0 0 12 12"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g fill="none" fillRule="evenodd">
-              <path
-                d="M1.5 5.2h4.8c.3 0 .5.2.5.4v5.1c-.1.2-.3.3-.4.3H1.4a.5.5 0 01-.5-.4V5.7c0-.3.2-.5.5-.5zm0-2.1h6.9c.3 0 .5.2.5.4v7a.5.5 0 01-1 0V4H1.5a.5.5 0 010-1zm0-2.1h9c.3 0 .5.2.5.4v9.1a.5.5 0 01-1 0V2H1.5a.5.5 0 010-1zm4.3 5.2H2V10h3.8V6.2z"
-                id="a"
-                fill="#999"
-              />
-            </g>
-          </svg>
-          Viewports addon in the toolbar
+      <Breadcrumbs>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="#" key="home">
+            ホーム
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="#" key="organization">
+            組織情報
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="#" key="a">
+            長いページタイトルが入ります長いページタイトルが入ります
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="#" key="b">
+            長いページタイトルが入ります長いページタイトルが入ります
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem className="text-oln-14N-1" isCurrent>
+          長いページタイトルが入ります長いページタイトルが入ります
+        </BreadcrumbItem>
+      </Breadcrumbs>
+      <Accordion>
+        <AccordionSummary className="p-2">
+          <h3>ダミーテキストはどのような場合に使用されますか。</h3>
+          <AccordionDefaultIcon />
+        </AccordionSummary>
+        <div className="px-2 py-4">
+          これはダミーテキストです。ダミーテキストは、デザインやレイアウトの作成時に使用される仮の文章です。ダミーテキストを使用すると、デザインの全体像を評価したり、テキストの配置や長さを確認したりすることができます。ダミーテキストは実際の文章ではないので、内容には意味がありません。
         </div>
-      </section>
+      </Accordion>
     </article>
   );
 };
